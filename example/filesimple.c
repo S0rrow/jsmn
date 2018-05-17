9a589da70d2b473ec48f4f925d643a9b032a973a
S0rrow <jae9hell@gmail.com>
2018-05-17 23:17:23 +0900

    filesimple.c added

--- example/filesimple.c
+++ example/filesimple.c
@@ -3,101 +3,39 @@
 #include <string.h>
 #include "../jsmn.h"
 
-/*
- * A small example of jsmn parsing when JSON structure is known and number of
- * tokens is predictable.
- */
+typedef struct filedata* pfiledata;
+typedef struct filedata{
+	int size;
+	char* data;
+}filedata;
 
-char *readJSONFile() {
-	FILE* fp = fopen("..\\data.json","r");
-	if(f == NULL) return NULL;
-	char* JSON_STRING;
-	char oneline[255];
-	while(!feof(fp)) {
-		cache = fgets(JSON_STRING, count, fp);
-		count = strlen(oneline);
-		(char*)realloc(sizeof(count));
-		strcat(JSON_STRING,)
+pfiledata readJSONFile() {
+	pfiledata data_json;
+	FILE* fp = fopen("data.json","r");
+	int size = 0;
+	while(!feof(fp)){
+		char cache[100];
+		fgets(cache, 100, fp);
+		size = size + strlen(cache);
 	}
-	return JSON_STRING;
 	fclose(fp);
+	data_json = (pfiledata)malloc(size+sizeof(int));
+	data_json->size = size;
+	FILE* fp2 = fopen("data.json","r");
+	char* oneline = (char*)malloc(size);
+	while(!feof(fp2)) {
+		char cache[100];
+		fgets(cache, 100, fp2);
+		strcat(oneline, cache);
+	}
+	fclose(fp2);
+	data_json->data = oneline;
+	free(oneline);
+	return data_json;
 }
 
-static const char *JSON_STRING = readJSONFile();
-	//"{\"user\": \"johndoe\", \"admin\": false, \"uid\": 1000,\n  "
-	//"\"groups\": [\"users\", \"wheel\", \"audio\", \"video\"]}";
-
-static int jsoneq(const char *json, jsmntok_t *tok, const char *s) {
-	if (tok->type == JSMN_STRING && (int) strlen(s) == tok->end - tok->start &&
-			strncmp(json + tok->start, s, tok->end - tok->start) == 0) {
-		return 0;
-	}
-	return -1;
-}
-
-int main() {
-	int i;
-	int r;
-	jsmn_parser p;
-	jsmntok_t t[128]; /* We expect no more than 128 tokens */
-
-	jsmn_init(&p);
-	r = jsmn_parse(&p, JSON_STRING, strlen(JSON_STRING), t, sizeof(t)/sizeof(t[0]));
-	if (r < 0) {
-		printf("Failed to parse JSON: %d\n", r);
-		return 1;
-	}
-
-	/* Assume the top-level element is an object */
-	if (r < 1 || t[0].type != JSMN_OBJECT) {
-		printf("Object expected\n");
-		return 1;
-	}
-
-	/* Loop over all keys of the root object */
-	for (i = 1; i < r; i++) {
-		if (jsoneq(JSON_STRING, &t[i], "Name") == 0) {
-			/* We may use strndup() to fetch string value */
-			printf("\"Name\": %.*s\n", t[i+1].end-t[i+1].start,
-					JSON_STRING + t[i+1].start);
-			i++;
-		} else if (jsoneq(JSON_STRING, &t[i], "keywords") == 0) {
-			/* We may additionally check if the value is either "true" or "false" */
-			printf("\"keywords\": %.*s\n", t[i+1].end-t[i+1].start,
-					JSON_STRING + t[i+1].start);
-			i++;
-		} else if (jsoneq(JSON_STRING, &t[i], "description") == 0) {
-			/* We may want to do strtol() here to get numeric value */
-			printf("\"description\": %.*s\n", t[i+1].end-t[i+1].start,
-					JSON_STRING + t[i+1].start);
-			i++;
-		} else if (jsoneq(JSON_STRING, &t[i], "exclude") == 0) {
-                        printf("\"exclude\": %.*s\n", t[i+1].end-t[i+1].start,
-                                        JSON_STRING + t[i+1].start);
-                        i++;
-		} else if (jsoneq(JSON_STRING, &t[i], "repository") == 0) {
-			int j;
-			printf("\"repository\":\n(");
-			if (t[i+1].type != JSMN_ARRAY) {
-				continue; /* We expect groups to be an array of strings */
-			}
-			for (j = 0; j < t[i+1].size; j++) {
-				jsmntok_t *g = &t[i+j+3];
-				if (jsoneq(JSON_STRING, &t[i+j+2], "type") == 0) {
-                       		 printf("\"type\": %.*s\n", g->end - g->start,JSON_STRING + g->start);
-				}
-				else if (jsoneq(JSON_STRING, &t[i+j+2], "url") == 0) {
-                                 printf("\"url\": %.*s\n", g->end - g->start,JSON_STRING + g->start);
-                                }
-				//jsmntok_t *g = &t[i+j+2];
-				//printf("  * %.*s\n", g->end - g->start, JSON_STRING + g->start);
-			}
-			printf("),");
-			i += t[i+1].size + 1;
-		} else {
-			printf("Unexpected key: %.*s\n", t[i].end-t[i].start,
-					JSON_STRING + t[i].start);
-		}
-	}
-	return EXIT_SUCCESS;
+int main(){
+	pfiledata JSONdata = readJSONFile();
+	printf("%s", JSONdata->data);
+	return 0;
 }
